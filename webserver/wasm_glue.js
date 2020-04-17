6a53a5a8bc71e47771af26b70b8758de8c528012
Exr0n <spotyie@gmail.com>
2020-04-16 17:58:03 -0700

    finally can import :sunglasses:

--- webserver/wasm_glue.js
+++ webserver/wasm_glue.js
@@ -1,14 +1,6 @@
-// https://github.com/mdn/webassembly-examples/issues/5
-var importObject = {
-  imports: { imported_func: arg => console.log(arg) }
-};
-
-WebAssembly.instantiateStreaming(fetch('/node_modules/@exr0n/wasm/wasm_bg.wasm'), importObject)
-.then(obj => console.log);
-
-// import wasm from "/node_modules/@exr0n/wasm/wasm.js";
-
-// console.log(js);
-// js.then(js => {
-//     js.greet("WebAssembly");
-// });
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WebAssembly/compileStreaming
+WebAssembly.compileStreaming(fetch('/node_modules/@exr0n/wasm/wasm_bg.wasm'))
+.then(function(mod) {
+  var imports = WebAssembly.Module.imports(mod);
+  console.log(imports[0]);
+});
