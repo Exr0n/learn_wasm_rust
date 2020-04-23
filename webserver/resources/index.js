33cc93b6016611374185b02ff37fd1b999d73497
Exr0n <spotyie@gmail.com>
2020-04-22 21:05:50 -0700

    https://stackoverflow.com/a/52732080 lied.

--- webserver/resources/index.js
+++ webserver/resources/index.js
@@ -1,8 +1,14 @@
-import js from '/wasm.js';
-
+// import js from '/wasm/wasm_src.js';
 // js.then(console.log);
 
-// WebAssembly.compileStreaming(fetch('/wasm/app.wasm'))
+const wasm = fetch('/wasm/wasm_src_bg.wasm')
+    .then(res => res.arrayBuffer())
+    .then(buf => WebAssembly.instantiate(buf))
+    .then(obj => obj.instance.exports);
+
+console.log(wasm);
+
+// WebAssembly.compileStreaming(fetch('/wasm/wasm_src_bg.wasm'))
 //     .then(mod => {
 //         const imports = WebAssembly.Module.imports(mod);
 //         console.log(imports[0]);
