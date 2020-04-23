53db02227ee7e8214772034e8c7d2702c24fd208
Exr0n <spotyie@gmail.com>
2020-04-23 13:47:17 -0700

    distinct not understanding noises

--- webserver/resources/index.js
+++ webserver/resources/index.js
@@ -1,12 +1,12 @@
-// import js from '/wasm/wasm_src.js';
+import js from '/wasm/wasm_src.js';
 // js.then(console.log);
 
-const wasm = fetch('/wasm/wasm_src_bg.wasm')
-    .then(res => res.arrayBuffer())
-    .then(buf => WebAssembly.instantiate(buf))
-    .then(obj => obj.instance.exports);
+//const wasm = fetch('/wasm/wasm_src_bg.wasm')
+//    .then(res => res.arrayBuffer())
+//    .then(buf => WebAssembly.instantiate(buf))
+//    .then(obj => obj.instance.exports);
 
-console.log(wasm);
+//console.log(wasm);
 
 // WebAssembly.compileStreaming(fetch('/wasm/wasm_src_bg.wasm'))
 //     .then(mod => {
